9cf48463f9baf4af1e09c48ea3a4be0b8305316e
Erik Berg <github@erikberg.com>
2016-02-17 05:37:17 -0800

    Move local functions to separate file.

---
+++ functions.php
@@ -0,0 +1,80 @@
+<?php
+
+// See https://erikberg.com/api/endpoints Request URL Convention for
+// an explanation
+function buildUrl($host, $sport, $endpoint, $id, $format, $parameters)
+{
+    $ary  = array($sport, $endpoint, $id);
+    $path = join('/', preg_grep('/^$/', $ary, PREG_GREP_INVERT));
+    $url  = 'https://' . $host . '/' . $path . '.' . $format;
+
+    // Check for parameters and create parameter string
+    if (!empty($parameters)) {
+        $paramlist = array();
+        foreach ($parameters as $key => $value) {
+            array_push($paramlist, rawurlencode($key) . '=' . rawurlencode($value));
+        }
+        $paramstring = join('&', $paramlist);
+        if (!empty($paramlist)) { $url .= '?' . $paramstring; }
+    }
+    return $url;
+}
+
+function httpGet($ua, $auth, $url)
+{
+    $options = [
+        CURLOPT_URL => $url,
+        CURLOPT_USERAGENT => $ua,
+        CURLOPT_ENCODING => 'gzip',
+        CURLOPT_HTTPHEADER => [ $auth ],
+        CURLOPT_FAILONERROR => 1,
+        CURLOPT_RETURNTRANSFER => true
+    ];
+
+    $ch = curl_init();
+    curl_setopt_array($ch, $options);
+    $response = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        echo '<pre>Curl error ', curl_errno($ch), '. ', curl_error($ch),'</pre>';
+        curl_close($ch);
+        exit();
+    }
+
+    curl_close($ch);
+    return $response;
+}
+
+function showDate($d)
+{
+    if ($d) {
+        echo $d->format('l, F j, Y');
+    }
+}
+
+function dateAdd($date, $diff)
+{
+    if ($date) {
+        $d = new DateTime($date->format('c'));
+        if ($diff > 0) {
+            $d->add(new DateInterval('P1D'));
+        } else {
+            $d->sub(new DateInterval('P1D'));
+        }
+        echo 'd=',$d->format('Ymd');
+    }
+}
+
+function showStatus($evt) {
+    if ($evt->event_status == 'completed') {
+        echo 'Final';
+    } else {
+        echo ucfirst($evt->event_status);
+    }
+}
+
+function save_cache($cache, $key, $data) {
+   $cache->add($key, $data, false, 600);
+}
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+include 'functions.php';
+
 $config = parse_ini_file('xmlstats.ini');
 
 $time_zone = $config['time_zone'];
@@ -46,6 +48,7 @@ $events = json_decode($response);
 
 // Create DateTime object using the ISO 8601 formatted events_date
 $date = DateTime::createFromFormat(DateTime::W3C, $events->events_date);
+
 ?>
 
 <!DOCTYPE html>
@@ -89,9 +92,6 @@ tfoot tr:first-child td {
 }
 tfoot tr:last-child td {
  padding-bottom:6px;
-}
-tbody td {
- text-align: left;
 }
 tbody td:first-child {
  max-width: 90%;
@@ -196,85 +196,3 @@ foreach ($events->event as $evt) :
 
 </body>
 </html>
-
-
-<?php
-
-// See https://erikberg.com/api/endpoints Request URL Convention for
-// an explanation
-function buildUrl($host, $sport, $endpoint, $id, $format, $parameters)
-{
-    $ary  = array($sport, $endpoint, $id);
-    $path = join('/', preg_grep('/^$/', $ary, PREG_GREP_INVERT));
-    $url  = 'https://' . $host . '/' . $path . '.' . $format;
-
-    // Check for parameters and create parameter string
-    if (!empty($parameters)) {
-        $paramlist = array();
-        foreach ($parameters as $key => $value) {
-            array_push($paramlist, rawurlencode($key) . '=' . rawurlencode($value));
-        }
-        $paramstring = join('&', $paramlist);
-        if (!empty($paramlist)) { $url .= '?' . $paramstring; }
-    }
-    return $url;
-}
-
-function httpGet($ua, $auth, $url)
-{
-    $options = [
-        CURLOPT_URL => $url,
-        CURLOPT_USERAGENT => $ua,
-        CURLOPT_ENCODING => 'gzip',
-        CURLOPT_HTTPHEADER => [ $auth ],
-        CURLOPT_FAILONERROR => 1,
-        CURLOPT_RETURNTRANSFER => true
-    ];
-
-    $ch = curl_init();
-    curl_setopt_array($ch, $options);
-    $response = curl_exec($ch);
-
-    if (curl_errno($ch)) {
-        echo '<pre>Curl error ', curl_errno($ch), '. ', curl_error($ch),'</pre>';
-        curl_close($ch);
-        exit();
-    }
-
-    curl_close($ch);
-    return $response;
-}
-
-function showDate($d)
-{
-    if ($d) {
-        echo $d->format('l, F j, Y');
-    }
-}
-
-function dateAdd($date, $diff)
-{
-    if ($date) {
-        $d = new DateTime($date->format('c'));
-        if ($diff > 0) {
-            $d->add(new DateInterval('P1D'));
-        } else {
-            $d->sub(new DateInterval('P1D'));
-        }
-        echo 'd=',$d->format('Ymd');
-    }
-}
-
-function showStatus($evt) {
-    if ($evt->event_status == 'completed') {
-        echo 'Final';
-    } else {
-        echo ucfirst($evt->event_status);
-    }
-}
-
-function save_cache($cache, $key, $data) {
-   $cache->add($key, $data, false, 600);
-}
-
-?>
